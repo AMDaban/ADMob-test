0db6823d1c2dfb8b8374da9660fe6234c994779c
Alireza <daban.am56@gmail.com>
2018-04-12 15:33:42 +0430

    interstitial ads added

--- app/src/main/java/com/test/admobtest/Constants.java
+++ app/src/main/java/com/test/admobtest/Constants.java
@@ -1,5 +1,14 @@
 package com.test.admobtest;
 
 class Constants {
+
+    /**
+     * ADMob addId
+     */
     static final String appId = "ca-app-pub-3940256099942544~3347511713";
+
+    /**
+     * Interstitial Ad Unit Id
+     */
+    static final String interstitialAdUnitId = "ca-app-pub-3940256099942544/1033173712";
 }
--- app/src/main/java/com/test/admobtest/MainActivity.java
+++ app/src/main/java/com/test/admobtest/MainActivity.java
@@ -3,15 +3,23 @@ package com.test.admobtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.ads.AdListener;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
 
 public class MainActivity extends AppCompatActivity {
 
     private final String TAG = "ADMobTest";
+
+    /**
+     * Interstitial Ad
+     */
+    private InterstitialAd interstitialAd = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
          *  initialize ADMob
          */
         MobileAds.initialize(this, Constants.appId);
+
+        /* banner ads */
 
         /*
          *  create a new banner ad
@@ -41,7 +51,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onAdFailedToLoad(int errorCode) {
                 // Code to be executed when an ad request fails.
-                Log.e(TAG, "Banner - onAdFailedToLoad");
+                Log.e(TAG, "Banner - onAdFailedToLoad - error code : " + errorCode);
             }
 
             @Override
@@ -70,5 +80,83 @@ public class MainActivity extends AppCompatActivity {
          */
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+
+        /* end of implementation of banner ads */
+
+        /* interstitial ads */
+
+        /*
+         *  show the interstitial ad
+         */
+        final Button showAd = findViewById(R.id.btnShowAd);
+        showAd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (interstitialAd.isLoaded()) {
+                    interstitialAd.show();
+                } else {
+                    Log.d(TAG, "Interstitial - The interstitial wasn't loaded yet.");
+                }
+            }
+        });
+        showAd.setEnabled(false);
+
+        interstitialAd = new InterstitialAd(this);
+        interstitialAd.setAdUnitId(Constants.interstitialAdUnitId);
+
+        interstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                // Code to be executed when an ad finishes loading.
+                Log.i(TAG, "Interstitial - onAdLoaded");
+
+                showAd.setEnabled(true);
+            }
+
+            @Override
+            public void onAdFailedToLoad(int errorCode) {
+                // Code to be executed when an ad request fails.
+                Log.i(TAG, "Interstitial - onAdFailedToLoad - error code : " + errorCode);
+
+                showAd.setEnabled(false);
+            }
+
+            @Override
+            public void onAdOpened() {
+                // Code to be executed when the ad is displayed.
+                Log.i(TAG, "Interstitial - onAdOpened");
+
+                showAd.setEnabled(false);
+            }
+
+            @Override
+            public void onAdLeftApplication() {
+                // Code to be executed when the user has left the app.
+                Log.i(TAG, "Interstitial - onAdLeftApplication");
+
+                showAd.setEnabled(false);
+            }
+
+            @Override
+            public void onAdClosed() {
+                // Code to be executed when when the interstitial ad is closed.
+                Log.i(TAG, "Interstitial - onAdClosed");
+
+                showAd.setEnabled(false);
+            }
+        });
+
+        Button requestInterstitialButton = findViewById(R.id.btnInterstitial);
+        requestInterstitialButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                /*
+                 *  request interstitial ad
+                 */
+                interstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
+        /* end of implementation of interstitial ads */
     }
 }
